84fe607b6ecd14497e427fb26f192afc98e9875b
kexin76 <kexincx@gmail.com>
2026-03-11 23:59:31 -0400

    Updated to include routes handlers

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,7 +1,10 @@
 import express from 'express';
 import users from './users/users-handlers.ts'
+import routes from './routes/routes-handlers.ts'
 const app = express()
 const port = 3000
+
+app.use(express.json())
 
 app.get('/', async (req, res) => {
   res.send("PLACEHOLDER")
@@ -13,3 +16,5 @@ app.listen(port, () => {
 
 
 app.use('/users',users)
+
+app.use('/routes',routes)
