1c3816d2dcb5ecd6673d82b2960b9916b3e0f90e
kexin76 <kexincx@gmail.com>
2026-03-12 00:03:10 -0400

    Created routes handler, queries, and service files. Constructed standard query and test to get route from OpenTripPlanner

---
+++ backend/src/routes/routes-queries.ts
@@ -0,0 +1,5 @@
+import { supabase } from '../config/supabase.ts'
+
+
+export async function postRoute(user_id, route){
+}
---
+++ backend/src/routes/routes-service.ts
@@ -0,0 +1,147 @@
+
+
+const graphql_url = "http://localhost:8080/otp/transmodel/v3"
+const query = `
+query trip($accessEgressPenalty: [PenaltyForStreetMode!], $alightSlackDefault: Int, $alightSlackList: [TransportModeSlack], $arriveBy: Boolean, $banned: InputBanned, $bicycleOptimisationMethod: BicycleOptimisationMethod, $bikeSpeed: Float, $boardSlackDefault: Int, $boardSlackList: [TransportModeSlack], $bookingTime: DateTime, $dateTime: DateTime, $filters: [TripFilterInput!], $from: Location!, $ignoreRealtimeUpdates: Boolean, $includePlannedCancellations: Boolean, $includeRealtimeCancellations: Boolean, $itineraryFilters: ItineraryFilters, $locale: Locale, $maxAccessEgressDurationForMode: [StreetModeDurationInput!], $maxDirectDurationForMode: [StreetModeDurationInput!], $maximumAdditionalTransfers: Int, $maximumTransfers: Int, $modes: Modes, $numTripPatterns: Int, $pageCursor: String, $relaxTransitGroupPriority: RelaxCostInput, $searchWindow: Int, $timetableView: Boolean, $to: Location!, $transferPenalty: Int, $transferSlack: Int, $triangleFactors: TriangleFactors, $useBikeRentalAvailabilityInformation: Boolean, $via: [TripViaLocationInput!], $waitReluctance: Float, $walkReluctance: Float, $walkSpeed: Float, $wheelchairAccessible: Boolean, $whiteListed: InputWhiteListed) {
+  trip(
+    accessEgressPenalty: $accessEgressPenalty
+    alightSlackDefault: $alightSlackDefault
+    alightSlackList: $alightSlackList
+    arriveBy: $arriveBy
+    banned: $banned
+    bicycleOptimisationMethod: $bicycleOptimisationMethod
+    bikeSpeed: $bikeSpeed
+    boardSlackDefault: $boardSlackDefault
+    boardSlackList: $boardSlackList
+    bookingTime: $bookingTime
+    dateTime: $dateTime
+    filters: $filters
+    from: $from
+    ignoreRealtimeUpdates: $ignoreRealtimeUpdates
+    includePlannedCancellations: $includePlannedCancellations
+    includeRealtimeCancellations: $includeRealtimeCancellations
+    itineraryFilters: $itineraryFilters
+    locale: $locale
+    maxAccessEgressDurationForMode: $maxAccessEgressDurationForMode
+    maxDirectDurationForMode: $maxDirectDurationForMode
+    maximumAdditionalTransfers: $maximumAdditionalTransfers
+    maximumTransfers: $maximumTransfers
+    modes: $modes
+    numTripPatterns: $numTripPatterns
+    pageCursor: $pageCursor
+    relaxTransitGroupPriority: $relaxTransitGroupPriority
+    searchWindow: $searchWindow
+    timetableView: $timetableView
+    to: $to
+    transferPenalty: $transferPenalty
+    transferSlack: $transferSlack
+    triangleFactors: $triangleFactors
+    useBikeRentalAvailabilityInformation: $useBikeRentalAvailabilityInformation
+    via: $via
+    waitReluctance: $waitReluctance
+    walkReluctance: $walkReluctance
+    walkSpeed: $walkSpeed
+    wheelchairAccessible: $wheelchairAccessible
+    whiteListed: $whiteListed
+  ) {
+    previousPageCursor
+    nextPageCursor
+    tripPatterns {
+      aimedStartTime
+      aimedEndTime
+      expectedEndTime
+      expectedStartTime
+      duration
+      distance
+      generalizedCost
+      legs {
+        id
+        mode
+        aimedStartTime
+        aimedEndTime
+        expectedEndTime
+        expectedStartTime
+        realtime
+        distance
+        duration
+        generalizedCost
+        fromPlace {
+          name
+          quay {
+            id
+          }
+        }
+        toPlace {
+          name
+          quay {
+            id
+          }
+        }
+        toEstimatedCall {
+          destinationDisplay {
+            frontText
+          }
+        }
+        line {
+          publicCode
+          name
+          id
+          presentation {
+            colour
+          }
+        }
+        authority {
+          name
+          id
+        }
+        pointsOnLink {
+          points
+        }
+        interchangeTo {
+          staySeated
+        }
+        interchangeFrom {
+          staySeated
+        }
+      }
+      systemNotices {
+        tag
+      }
+    }
+  }
+}
+`
+
+    
+
+
+export async function getRoute(origin, destination, extra){
+  console.log('get route function')
+  const data = await fetch(graphql_url, {
+    method: "POST",
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      query: query,
+      variables: {
+        from:{coordinates: { latitude: 40.59016932131942, longitude: -73.97384273780594}},
+        to: {coordinates: {latitude: 40.76772735386152,longitude: -73.96445880303273}},
+        dateTime: new Date(),
+        modes: 
+          { accessMode: "foot",
+            transportModes: [{transportMode: "bus"},{transportMode: "metro"}],
+            egressMode: "foot"
+          },
+        numTripPatterns: 1
+      }
+    })
+  })
+  console.log(data)
+  const data_body = await data.json()
+  console.log(data_body)
+  console.log(data_body["data"]["trip"]["tripPatterns"])
+
+}
+
+
