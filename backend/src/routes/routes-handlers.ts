b277c5e4d6061feaf6bf6870e022cd7b10bf2c9f
kexin76 <kexincx@gmail.com>
2026-03-13 22:01:51 -0400

    updated routes files

--- backend/src/routes/routes-handlers.ts
+++ backend/src/routes/routes-handlers.ts
@@ -5,21 +5,14 @@ import { getRoute } from './routes-service.ts'
 import { postRoute } from './routes-queries.ts'
 
 
-
-router.post('/test', async (req,res) => {
-    await getRoute("asd", "asd", "asd")
-    res.send("checking test")
-})
-
 router.post('/make', async (req,res) => {
     const {user_id, origin, destination, extra} = req.body
-    if (!user_id || !origin || !destination){
+    if (!user_id || !origin || !destination || !extra){
         throw "Required field(s) missing"
     }
+    await getRoute(origin,destination,extra)
 
-    const route_json = getRoute(origin,destination,extra)
-
-    await postRoute(user_id,route_json)
+    // await postRoute(user_id,route_json)
 
 
     res.send('Got a POST request')
