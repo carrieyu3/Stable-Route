ed23f3cd2ea08bd26b759388d23a0717f7c648da
kexin76 <kexincx@gmail.com>
2026-03-15 16:36:39 -0400

    Created ValidUserID function

--- backend/src/users/users-queries.ts
+++ backend/src/users/users-queries.ts
@@ -1,10 +1,11 @@
 import { supabase } from '../config/supabase.ts'
 
-
-export async function fetchUsers() {
-   const { data, error } = await supabase.from('User').select('*')
+export async function validUserID(user_id:string){
+    const {data, error} = await supabase.from("User").select().eq('user_id', user_id).maybeSingle()
     if (error){
-        throw error
+        throw new Error(error["message"])
+    }else if(!data){
+        throw new Error("USER DOES NOT EXIST")
     }
-    return data
+    return
 }
