80b69e9f8b0a9b2638575f6d1576459337a5c2fd
7ronbon <7ronbon@gmail.com>
2026-03-24 18:42:19 -0400

    fixed setting button and added feature to get user location however it needs to feed the infomration into starting point

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -3,36 +3,76 @@ import { useForm } from "react-hook-form";
 import type { SubmitHandler } from "react-hook-form";
 import './Home.css'
 
-// import maplibregl from 'maplibre-gl' 
-
 //library import needed for map
-import Map from 'react-map-gl/maplibre';
+import {Map , GeolocateControl } from 'react-map-gl/maplibre';
 import 'maplibre-gl/dist/maplibre-gl.css';
 
+//icons 
+import { GearFill } from 'react-bootstrap-icons';
+import { useEffect, useRef , useState } from "react";
+
+
+// input for starting destination 
+type destinationInput = {
+  origin: string;
+  destination: string;
+}
 
 
 export default function MainPG(){
-    
-    // input for starting destination 
-    type destinationInput = {
-        startPoint: string;
-        endPoint: string;
-    }
 
+  const [addressError, setAddressError] = useState("");
+
+  //database errors - override supabase default message
+    const displayAddressError = (error: any) => {
+        if (error.message.includes("Invalid address")) {
+            setAddressError("Invalid address");
+        }
+    };
+    
     //create form and track input data upon submission
         const {
-            handleSubmit,
+          register,
+          handleSubmit,
+          clearErrors,
+          formState: { errors } ,
         } = useForm<destinationInput>({mode: "onSubmit",});
 
-        // check if input is valid
-        //see if address is valid 
+  //Capture user input and begin Signup validation
+  const onSubmit: SubmitHandler<destinationInput> = async (data) => {
+    if (data.origin == data.destination){
+      displayAddressError("Invalid address.");
+    }
+    else if(data.origin == "" || data.destination == ""){
+      displayAddressError("Invalid address");
+    }
+    else{
+      const rawResponse = await fetch('https://localhost:3000', {
+        method: 'POST',
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+          },
+        body: JSON.stringify({origin: 1, destination: 2})
+    });
+    }
+    return;
+  }
 
-        const onSubmit: SubmitHandler<destinationInput> = () => {}
+      // check if input is valid
+      //see if address is valid 
+
+      //get user curr location
+      const geoControlRef= useRef<maplibregl.GeolocateControl>(null)  
+
+      useEffect(()=>{
+        geoControlRef.current?.trigger()
+      },[geoControlRef.current])
 
     return(
       <>
 
-        <div className="flex min-h-full flex-col justify-center">
+        <div className="flex min-h-full flex-col justify-center absolute">
 
         { /* insert map here */}
           <Map
@@ -40,33 +80,53 @@ export default function MainPG(){
               longitude: -74.0060,
               latitude: 40.7128,
               zoom: 12
+
             }}
             style={{width: '100vw', height: '100vh'}}
             mapStyle="src/assets/americana.json"
-          />
+          >
 
-        { /* input for starting destinaton */}
-          <form onSubmit={handleSubmit(onSubmit)} className="space-y-6">
-            <div className="sm:mx-auto sm:w-full sm:max-w-sm absolute left-6 top-6">  
+            {/* below gives us the user location , we want to feed it into start point */}
+            {/* <GeolocateControl showUserLocation={true} trackUserLocation={true}/> */}
+
+          </Map>
+
+            
+        { /* input for destinaton */}
+          <form onSubmit={handleSubmit(onSubmit)}>
+            
+            <div className="mx-auto w-full max-w-sm absolute left-6 top-6 flex flex-col">  
 
               {/* starting point input */}
               <input
-                id = "startPoint"
+                id = "origin"
                 placeholder = "start point"
                 className="block w-full rounded-md border-2 border-gray-400 bg-white px-3 py-1.5 text-base text-black placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-blue-500 sm:text-sm/6"
+
+                {...register("origin", { required: true })}
+
+                onChange={() => {setAddressError("") ; clearErrors("origin"); }}
+                
               />
 
               {/* ending point input */}
               <input
-                id = "endPoint"
+                id = "destination"
                 placeholder = "end point"
                 className="block w-full rounded-md border-2 border-gray-400 bg-white px-3 py-1.5 text-base text-black placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-blue-500 sm:text-sm/6"
+
+                {...register("destination", { required: true })}
+
+                onChange={() => {setAddressError("") ; clearErrors("destination"); }}
               />
-               
-              <button type="submit">settings</button>
-              
+
             </div>
 
+            <div className="max-w-sm absolute right-6 top-6">
+              <a href="/preference" className="w-100 h-100 p-2 bg-slate-800 text-white text-sm font-medium rounded-md hover:bg-slate-700 transition-colors">settings</a>
+              {/* using <GearFill/> doesnt work for some reason */}
+            </div>
+            
           </form>
 
         </div>
