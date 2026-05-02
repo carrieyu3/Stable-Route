1ad77d305c94b2066a7281a06d05ec60fd2cb79e
Carrie Yu <carrieyu@Mac.fios-router.home>
2026-05-01 22:35:19 -0400

    Fixed Route policy issue

--- backend/src/config/supabase.ts
+++ backend/src/config/supabase.ts
@@ -8,16 +8,16 @@ const env_path = join(__dirname,'../../.env')
 
 dotenv.config({path: env_path})
 const SUPABASE_URL = process.env.SUPABASE_URL
-const SUPABASE_PUBLISHABLE_KEY = process.env.SUPABASE_PUBLISHABLE_KEY
+const SUPABASE_SERVICE_ROLE_KEY = process.env.SUPABASE_SERVICE_ROLE_KEY
 
 if (!SUPABASE_URL){
     throw new Error('Supbase Url is undefined')
 }
-if (!SUPABASE_PUBLISHABLE_KEY){
-    throw new Error('Supabase Pusblishable Key is undefined')
+if (!SUPABASE_SERVICE_ROLE_KEY){
+    throw new Error('Supabase Service Role Key is undefined')
 }
 
-const supabase = createClient(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY)
+const supabase = createClient(SUPABASE_URL, SUPABASE_SERVICE_ROLE_KEY)
 
 export { supabase };
 
